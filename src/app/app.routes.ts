b2550a89147b85631e0cda348495297b09fe969c
ratnakarreddy641 <ratnu641@gmail.com>
2025-09-21 04:04:49 +0530

    routre fix2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { Admin } from './admin/admin';
 export const routes: Routes = [
 
     { path: '', component: Home },
-    { path: 'feedback', component: Feedback },
+    { path: 'feedback/:userId', component: Feedback },
     { path: 'admin', component: Admin },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
